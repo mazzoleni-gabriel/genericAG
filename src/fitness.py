323415a8a740cacda07b73b2bcd28d3653fbacdb
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2019-04-01 15:41:48 -0300

    radio problem

--- src/fitness.py
+++ src/fitness.py
@@ -1,11 +1,12 @@
 
-def calculate(function, cromossome):
-	return function(cromossome)
-
 #return a list of tuples (cromossome, evaluation)
 def evaluate(function, population):
 	evaluations = []
 	for i in range(len(population)):
-		value = calculate(function, population[i])
+		value = __calculate(function, population[i])
 		evaluations.append((population[i], value))
 	return evaluations
+
+
+def __calculate(function, cromossome):
+	return function(cromossome)
